b8f713fc47e9c3e99e79c197b3e93048a7569462
k-laughman <klaughman@cetechllc.com>
2021-08-26 09:54:50 -0400

    added testviconnect script

---
+++ testviconnect.ps1
@@ -0,0 +1,26 @@
+# Keith Laughman
+# www.cloudmethodsllc.com
+# Test CodeBuild script to validate network connection and output
+
+param(
+    [String]$VCenter="hci-vcenter.cetech-ne.local",
+    [String]$ClusterName="NTAP",
+    [String]$DatastoreCluster="NetApp-HCI-Datastore",
+    [String]$VMNetwork="VM_Network",
+    [String]$VMName="cetech-amzn2",
+    [String]$DiskFormat="Thin",
+    [String]$Folder="Templates",
+    [String]$clibName="cetech-images",
+    [String]$clibItemName="cetech-amzn2-seed"
+)
+
+# Import PowerCLI Modules
+Import-Module VMware.VimAutomation.Core
+
+# Connect to VCenter (Prompt for user credentials)
+Write-Output "Connect to VCenter"
+Connect-VIServer $VCenter -User administrator@vsphere.local -Password CETechPass123!
+
+# vSphere Cluster + Network configuration parameters
+$Cluster = Get-Cluster -Name $ClusterName
+Write-Output "Successful Connection to:" $Cluster
